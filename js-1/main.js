89b4881f649fb1c3abdfb330244b3ca5313adb17
AliBakhtiarii <142569816+AliBakhtiarii@users.noreply.github.com>
2023-08-20 14:54:23 +0330

    Add files via upload

---
+++ js-1/main.js
@@ -0,0 +1,6 @@
+let firstName = prompt('whats your first name?');
+let lastName = prompt('whats your last name?');
+let numberOfFirstName = firstName.length;
+let numberOfLastName = lastName.length;
+let numberOfFullName = (numberOfFirstName + numberOfLastName)
+alert(' number of your name are ' + numberOfFullName)
