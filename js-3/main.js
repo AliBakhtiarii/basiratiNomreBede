9c21e850f7106b2d10245a98efab0b95e192938a
AliBakhtiarii <142569816+AliBakhtiarii@users.noreply.github.com>
2023-08-23 12:16:09 +0330

    Add files via upload

---
+++ js-3/main.js
@@ -0,0 +1,51 @@
+// let number = prompt('use number');
+// let isPrime = true;
+
+// for(let i = 2; i< number;i++){
+//     if( number % i == 0 ){
+//         isPrime = false;
+//     }
+// }
+// if(isPrime){ 
+//     console.log('avale!!')
+// } else{
+//     console.log('morakabe!')
+// }
+//////////////////////////////////////
+
+// let character = prompt('ye chizi vared kon');
+//  let numberOfCharacter = character.length;
+//  let reverce = ''
+// for(let i = numberOfCharacter-1; i >= 0; i-- ){
+//      reverce = reverce + character[i]
+// }
+// console.log(reverce)
+///////////////////////////////////////////
+
+// let number = prompt('use number');
+// if(number > 0){
+//     for (let i = 1; i <= number; i++) {
+//       for (let j = i + 1; j <= number; j++) {
+//         for (let k = j + 1; k <= number; k++)
+//             if (i * i + j * j == k * k) {
+//                 console.log(i, j, k)
+//             } else {
+
+//             }
+//     }
+// }}
+
+///////////////////////////////////////////
+
+// let character = prompt('ye chizi vared kon');
+// let numberOfCharacter = character.length - 1;
+// let repeat ='';
+// for( let i = 0; i <= numberOfCharacter; i++ ){
+//     repeat = repeat + character[i] + character[i]
+// }
+// console.log(repeat)
+////////////////////////////////////////////
+
+
+
+
